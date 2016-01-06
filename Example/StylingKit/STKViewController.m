ed2f3bff65c24e0340222b1c879fc7a7d3612fca
Anton Matosov <anton.matosov@gmail.com>
2016-01-06 00:54:48 -0800

    Added button and label for test project Button shows alertview

--- Example/StylingKit/STKViewController.m
+++ Example/StylingKit/STKViewController.m
@@ -1,9 +1,21 @@
-//
-//  STKViewController.m
-//  StylingKit
+/*
+ * Copyright 2015-present StylingKit Development Team. All rights reserved..
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 //
 //  Created by Anton Matosov on 12/08/2015.
-//  Copyright (c) 2015 Anton Matosov. All rights reserved.
 //
 
 #import "STKViewController.h"
@@ -14,16 +26,15 @@
 
 @implementation STKViewController
 
-- (void)viewDidLoad
+- (IBAction)showAlertView
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Title"
+                                                        message:@"Message"
+                                                       delegate:nil
+                                              cancelButtonTitle:@"Cancel"
+                                              otherButtonTitles:@"OK", nil];
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [alertView show];
 }
 
 @end
